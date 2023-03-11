a6860ddeac31034d0b4d4efa37d588a44e3251fb
Catalina <ekaterinkagrin@gmail.com>
2023-03-11 20:10:43 -0300

    loop update

--- js/loop.js
+++ js/loop.js
@@ -5,12 +5,17 @@ const buttonClear = document.querySelector('#buttonClear');
 const tasksList = document.querySelector('#listOfTasks');
 const alertMessage = document.querySelector('#alert');
 
-// Когда задач нет - серое уведомление о том, что задачи отсутствуют, а кнопка «Очистить список» должна быть неактивна. 
+// Когда задач нет - серое уведомление о том, что задачи отсутствуют, а кнопка «Очистить список» должна быть неактивна. При добавлении задачи в список, каждая из них должна появляться в списке задач и напротив иметь неактивный чекбокс & чекбокс напротив задачи должен менять своё состояние при клике (говоря нам, что задача выполнена). 
 buttonAdd.addEventListener('click', function (event) {
     event.preventDefault(); 
 
     let taskText = message.value;
-    let taskHtml = `<li class="numberOfTask"> ${taskText} </li>`;
+    let taskHtml = `<li class="numberOfTask"> ${taskText} 
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                        <label class="form-check-label" for="inlineCheckbox1">Done</label>
+                    </div>
+                    </li>`;
     tasksList.insertAdjacentHTML('beforeend', taskHtml);
 
     if (tasksList.children.length > 0) {
@@ -24,12 +29,6 @@ buttonAdd.addEventListener('click', function (event) {
     message.value = "";
     
 });
-
-//- При добавлении задачи в список, каждая из них должна появляться в списке задач и напротив иметь неактивный чекбокс, а кнопка «Очистить список» должна быть активна. 
-
-
-
- 
 
 
 
